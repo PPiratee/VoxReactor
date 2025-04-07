1cedc0e101e308c08358e754045ab5aaa6901463
PPirate <ppiratevam@gmail.com>
2025-04-06 22:38:33 -0400

    wip

--- src/character/managers/BlushManager.cs
+++ src/character/managers/BlushManager.cs
@@ -47,6 +47,7 @@ namespace PPirate.VoxReactor
             if (isBLushing) {
                 return;
             }
+            glancePlugin.LoadPresetShy();
 
             isBLushing = true;
             currentSpeed = blushSpeed;
@@ -76,6 +77,7 @@ namespace PPirate.VoxReactor
             if (isDone && isDone2) {
                 if (!isBLushing) {
                     pendingDeBlush = false;
+                    glancePlugin.LoadPresetDefault();
                 }
                 isBLushing = false;
                 character.main.RemoveFixedDeltaTimeConsumer(BlushUpdate);
